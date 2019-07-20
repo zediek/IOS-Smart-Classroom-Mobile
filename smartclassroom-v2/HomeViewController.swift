8ee949253e65836f03e839ca097f115287acae8d
Zediek <zediekvillamil@gmail.com>
2019-07-20 12:03:47 +0800

    update HomeView Controller

--- smartclassroom-v2/HomeViewController.swift
+++ smartclassroom-v2/HomeViewController.swift
@@ -12,6 +12,7 @@ struct JSONData {
     let room_id: Int
     let add_device: Bool
     let room_name: String
+    let devices: Array<Any>
     
     
     enum SerializationError:Error {
@@ -25,9 +26,12 @@ struct JSONData {
         guard let add_device = json["add_device"] as? Bool else {throw SerializationError.missing("add_device is missing")}
         guard let room_name = json["room_name"] as? String else {throw SerializationError.missing("room_name is missing")}
         
+       guard let devices = json["devices"] as? Array<Any> else {throw SerializationError.missing("devices is missing")}
+        
         self.room_id = room_id
         self.add_device = add_device
         self.room_name = room_name
+        self.devices = devices
     }
     
     
@@ -44,6 +48,18 @@ class HomeViewController: UIViewController {
     var airconStatus: String = ""
     var tempStatus: String = ""
     var lightsStatus: String = ""
+    
+    
+    var DeviceData = [
+    
+        "class_name": "",
+        "device_name": "",
+        "remote_design": "",
+        "device_status": false,
+        "remote_design_id": 0,
+        "device_id": 0,
+        "room_status_id": 0
+        ] as [String : Any]
     
     
     
@@ -101,7 +117,22 @@ class HomeViewController: UIViewController {
                     for datas in room_status{
                         
                         if let JD = try? JSONData(json: datas){
-                            print(JD.room_name)
+                           // print(JD.room_name)
+                            if let Devices = JD.devices as? [[String: Any]]{
+                                
+                                for Device in Devices{
+                                    self.DeviceData["class_name"] = Device["class_name"] as? String
+                                    self.DeviceData["device_name"] = Device["device_name"] as? String
+                                    self.DeviceData["remote_design"] = Device["remote_design"] as? String
+                                    self.DeviceData["device_status"] = Device["device_status"] as? Bool
+                                    self.DeviceData["remote_design_id"] = Device["remote_design_id"] as? Int
+                                    self.DeviceData["device_id"] = Device["device_id"] as? Int
+                                    self.DeviceData["room_status_id"] = Device["room_status_id"] as? Int
+                                    
+                                    
+                                    print(self.DeviceData)
+                                }
+                            }
                         }
                         
                     }
